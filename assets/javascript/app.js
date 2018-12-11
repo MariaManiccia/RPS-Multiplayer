0ad336e241179856c29dd0f3d0a30c17fd7590bc
Maniccia <mariamaniccia@gmail.com>
2018-12-11 09:46:35 -0500

    start

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+
+
+var config = {
+    apiKey: "AIzaSyBCHAH13jt6jYCm2LHC1axBh6xF7e9Fw8Q",
+    authDomain: "rps-multiplayer-76118.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-76118.firebaseio.com",
+    projectId: "rps-multiplayer-76118",
+    storageBucket: "",
+    messagingSenderId: "543684742480"
+};
+
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
